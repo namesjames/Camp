f3b0fdec2606bd065a59282d3541e7364fc0c5e0
J ª/\/\ ∑ $ <james@galvanize.com>
2016-03-18 23:13:58 -0600

    Create practice.js

---
+++ Front-End/javascript/practice.js
@@ -0,0 +1,29 @@
+// JS Practice
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+var myStr = "\\\ \t \t \r \n";
+
+/*
+\'	single quote
+\"	double quote
+\\	backslash
+\n	new line
+\r	carriage return
+\t	tab
+\b	backspace
+\f	form feed
+*/
+
+// Concatenation
+var myStr = "This is the start. " + "This is the end.";
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+var myName = "James";
+var myStr = "My name is " + myName + " and I am swell!";
+
+var someAdjective = "awesome!";
+var myStr = "Learning to code is " 
+myStr += someAdjective;
