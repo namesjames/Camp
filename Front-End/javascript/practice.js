f91772ab1230b341bdbb6e6db8c105af59d0c578
J ª/\/\ ∑ $ <james@galvanize.com>
2016-03-27 22:40:45 -0600

    Update practice.js

--- Front-End/javascript/practice.js
+++ Front-End/javascript/practice.js
@@ -106,3 +106,23 @@ function minusSeven(num) {
 function timesFive(num){
  return num * 5;
 }
+
+var processed = 0;
+
+function process(num) {
+  return (num + 3) / 5;
+}
+
+processed = process(7);
+
+function queue(arr, item) {
+  arr.push(item);
+  var removed = arr.shift();
+  return removed;
+}
+
+var testArr = [1,2,3,4,5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(queue(testArr, 6)); 
+console.log("After: " + JSON.stringify(testArr));
