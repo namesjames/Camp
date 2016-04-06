08a3c813eec32054117c851e880af1bd45fdbe59
J ª/\/\ ∑ $ <james@galvanize.com>
2016-04-05 21:45:03 -0600

    Update practice.js

--- Front-End/javascript/practice.js
+++ Front-End/javascript/practice.js
@@ -211,3 +211,28 @@ function myTest(val) {
   return "Between 5 and 10";
   }
 }
+
+function myTest(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+function myTest(num) {
+  if (num < 5) {
+  return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else if (num >= 20) {
+      return "Huge";
+  }
+}
+
