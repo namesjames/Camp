63fa1d60703daf07c63c088f578d45d489c7a8a5
J ª/\/\ ∑ $ <james@galvanize.com>
2016-04-11 23:42:28 -0600

    Update practice.js

--- Front-End/javascript/practice.js
+++ Front-End/javascript/practice.js
@@ -236,17 +236,9 @@ function myTest(num) {
   }
 }
 
-switch (val) {
-    case 1: 
-      answer = "alpha";
-      break;
-    case 2:
-      answer = "beta";
-      break;
-    case 3:
-      answer = "gamma";
-      break;
-    case 4:
-      answer = "delta";
-    break;
-  }
+function isLess(a, b) {
+  return (a < b); 
+}
+isLess(10, 15);
+
+
