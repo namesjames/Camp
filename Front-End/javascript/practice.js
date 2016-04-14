43392651bc5b5bcb8c5388414866ab933fcb5241
J ª/\/\ ∑ $ <james@galvanize.com>
2016-04-13 23:34:25 -0600

    Update practice.js

--- Front-End/javascript/practice.js
+++ Front-End/javascript/practice.js
@@ -241,4 +241,43 @@ function isLess(a, b) {
 }
 isLess(10, 15);
 
+function myTest(val) {
 
+var answer = "";
+
+  switch (val) {
+    case 1: 
+      answer = "alpha"; 
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+    break;
+  }  
+  return answer;
+} 
+
+function switchOfStuff(val) {
+  var answer = "";
+  
+  switch (val) {
+  case "a":
+    answer = "apple";
+    break;
+  case "b":
+    answer = "bird";
+    break;
+  case "c":
+    answer = "cat";
+    break;
+  default:
+    answer = "stuff";
+}
+  
+  return answer;  
+}
