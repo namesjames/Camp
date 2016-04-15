ef6307bb8c0e446dc7c383b70b3625eebc25e1e0
J ª/\/\ ∑ $ <james@galvanize.com>
2016-04-14 23:40:51 -0600

    Update practice.js

--- Front-End/javascript/practice.js
+++ Front-End/javascript/practice.js
@@ -281,3 +281,16 @@ function switchOfStuff(val) {
   
   return answer;  
 }
+
+function abTest(a, b) {
+  if (a < 0) {
+    return undefined;
+  }
+  if (b < 0) {
+    return undefined;
+  }
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(-2,2);
