04c1cce2644ebc45f71d626911959530d9e6d127
J ª/\/\ ∑ $ <james@galvanize.com>
2016-04-17 23:59:18 -0600

    Update practice.js

--- Front-End/javascript/practice.js
+++ Front-End/javascript/practice.js
@@ -310,3 +310,13 @@ var testObj = {
 
 var hatValue = testObj.hat;
 var shirtValue = testObj.shirt;
+
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+var entreeValue = testObj["an entree"];
+var drinkValue = testObj["the drink"];
+
