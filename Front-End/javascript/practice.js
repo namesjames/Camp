e4f9016add28897345263a7c7795bd5aedfcc991
J ª/\/\ ∑ $ <james@galvanize.com>
2016-04-20 23:25:12 -0600

    Update practice.js

--- Front-End/javascript/practice.js
+++ Front-End/javascript/practice.js
@@ -389,3 +389,17 @@ var lookup = {
 
   return result = lookup[val];
 }
+
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+  if (myObj.hasOwnProperty(checkProp)) {
+return myObj[checkProp];
+  } else {
+return "Not Found";
+  }
+}
