6412bf4a790c0ff3d6eb07403b926b0a18ce939d
J ª/\/\ ∑ $ <james@galvanize.com>
2016-04-27 23:36:26 -0600

    Update practice.js

--- Front-End/javascript/practice.js
+++ Front-End/javascript/practice.js
@@ -438,3 +438,42 @@ var myPlants = [
 ];
 
 var secondTree = myPlants[1].list[1];
+
+var collection = {
+    2548: {
+      album: "Slippery When Wet",
+      artist: "Bon Jovi",
+      tracks: [ 
+        "Let It Rock", 
+        "You Give Love a Bad Name" 
+      ]
+    },
+    2468: {
+      album: "1999",
+      artist: "Prince",
+      tracks: [ 
+        "1999", 
+        "Little Red Corvette" 
+      ]
+    },
+    1245: {
+      artist: "Robert Palmer",
+      tracks: [ ]
+    },
+    5439: {
+      album: "ABBA Gold"
+    }
+};
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+function updateRecords(id, prop, value) {
+if (prop === "tracks" && value !== "") {
+    collection[id][prop].push(value);
+  } else if (value !== ""){
+    collection[id][prop] = value;
+  } else {
+    delete collection[id][prop];
+  }
+
+  return collection;
+}
