5db48388b354ea666620357e28998ca41509d994
J ª/\/\ ∑ $ <james@galvanize.com>
2016-05-08 23:32:56 -0600

    Update practice.js

--- Front-End/javascript/practice.js
+++ Front-End/javascript/practice.js
@@ -496,3 +496,15 @@ for (var i = 9; i > 0; i-=2) {
   myArray.push(i);
 }
 
+
+function multiplyAll(arr) {
+  var product = 1;
+  for (var i=0; i < arr.length; i++) {
+  for (var j=0; j < arr[i].length; j++) {
+  product = (arr[i][j] * product);
+  }
+}
+  return product;
+}
+
+multiplyAll([[1,2],[3,4],[5,6,7]]);
