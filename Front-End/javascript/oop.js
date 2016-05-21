3ef189e5d842a3627f7153d8dcff76f312f9f762
J ª/\/\ ∑ $ <james@galvanize.com>
2016-05-20 23:48:07 -0600

    Update oop.js

--- Front-End/javascript/oop.js
+++ Front-End/javascript/oop.js
@@ -103,3 +103,8 @@ var string = "Split me into an array";
 var array = [];
 
 array = string.split(' ');
+
+var joinMe = ["Split","me","into","an","array"];
+var joinedString = '';
+
+joinedString = joinMe.join(' ');
